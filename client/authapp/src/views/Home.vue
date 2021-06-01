97f49291c120ba4a6c233dc84cc0cb235a80b7f8
Bruno610339 <bmiguelgomes@ipvc.pt>
2021-06-01 18:19:05 +0100

    menu

--- client/authapp/src/views/Home.vue
+++ client/authapp/src/views/Home.vue
@@ -1,39 +1,6 @@
 <template>
   <div>
-	<!-- App Menu -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/home">Home</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-		  
-		  <a class="navbar-brand" href="/operador">Operador</a>
-
-		  
-        <div
-          class="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" @click="logUserOut">Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-	  
-	<!-- SideBar Menu 
-	<side-bar-component />-->
+    <NavBar />
 
     <section>
       <div class="container mt-5">
@@ -50,12 +17,12 @@
   </div>
 </template>
 <script>
-
+import NavBar from "@/components/auth/navBar.vue";
 import VueJwtDecode from "vue-jwt-decode";
 export default {
-  /*components: {
-	  SideBarComponent
-  },*/
+  components: {
+	  NavBar
+  },
   data() {
     return {
       user: {}
