1a70134e19dae50ce8e98e41b92c6abaf6898a55
joaoboas98 <joaoboas@ipvc.pt>
2021-06-02 18:53:05 +0100

    header

---
+++ client/authapp/src/components/auth/footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-footer  elevation="4" color="#383A3C">
+      <v-col class="text-center white--text" cols="12" >
+        {{ new Date().getFullYear() }} — <strong>Valomar</strong>
+      </v-col>  
+      <v-col class="text-center" cols="12" >
+      
+      </v-col>
+  </v-footer>
+</template>
+<script>
+  export default {
+    name: 'footer',
+    data: () => ({
+    }),
+    methods: {
+    },
+  }
+</script>
+
+<style scoped>
+.fixarRodape {
+        bottom: 0;
+        position: fixed;
+        width: 90%;
+        text-align: center;
+    }
+</style>
